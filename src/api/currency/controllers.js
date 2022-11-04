afb0fc5d00f8e91fc8ad751e3537bfbd42c8266b
Kevin Gonzalez <kevjetsky@gmail.com>
2022-11-03 22:50:16 -0600

    Change variable names to english

--- src/api/currency/controllers.js
+++ src/api/currency/controllers.js
@@ -4,15 +4,15 @@ import { pushArraysToData } from '../../libs/pushArrayData.js';
 let currency=[];
 let price = [];
 let oldCurrency = [];
-let change = [];
-var salidaCurrency ; 
+// let change = [];
+var currencyOut ; 
 var salidaChange  = [];
 
 export default {
     getLocalValue(req, res) { 
         return res.status(200).json(
             {
-                Rates: salidaCurrency,
+                Rates: currencyOut,
                 Variations: salidaChange
             }
         ); // armo mi array con objs de salida
@@ -30,7 +30,8 @@ export default {
             .then((body) => {
                 const $ = body;
 
-                currency=[]; //limpio para q el push no me acumule
+                currency = []; // limpio para q el push no me acumule
+                price = []; // limpio para q el push no me acumule     
 
                 $('span.currency','table.hmQVUs').each(function() {
                     var moneda = $(this).text();
@@ -38,35 +39,32 @@ export default {
                     currency.push(moneda);
                 }); // saco los span q tienen el texto de la moneda y lo limpio
 
-                price=[]; //limpio para q el push no me acumule     
-
                 $('span.price-text','table.hmQVUs').each(function() {
                     var precio = $(this).text();
                     precio = (precio.replace(' CUP', ''));
                     price.push(precio);
                 }); // saco los span q tienen el texto del valor respecto al cup  y lo limpio
 
-                salidaCurrency= pushArraysToData(currency, price); // armo mi array de salida
-                var nameMoneda= Object.keys(salidaCurrency);
+                currencyOut = pushArraysToData(currency, price); // armo mi array de salida
+                var currencyName = Object.keys(currencyOut);
                 
                 //compatibilizo los length del array
-                    for (let i = 0; i < nameMoneda.length; i++) {
-                       
-                        if(oldCurrency.hasOwnProperty(nameMoneda[i]))
-                        {  console.log('existe ' + nameMoneda[i] + ' en oldcurrency');
-                            if( salidaCurrency[nameMoneda[i]]!==oldCurrency[nameMoneda[i]]){ //solo si vario el precio en esta consulta respecto a la ultima consulta
-                                console.log(nameMoneda[i] + ' vario de precio');
-                                salidaChange[nameMoneda[i]]=salidaCurrency[nameMoneda[i]]-oldCurrency[nameMoneda[i]]; //encuentro la diferencia por tipo moneda
-                                oldCurrency[nameMoneda[i]]=salidaCurrency[nameMoneda[i]]; //paso el nuevo valor al old para la nueva consulta
+                    for (let i = 0; i < currencyName.length; i++) {
+                        if(oldCurrency.hasOwnProperty(currencyName[i]))
+                        {  console.log('existe ' + currencyName[i] + ' en oldcurrency');
+                            if( currencyOut[currencyName[i]]!==oldCurrency[currencyName[i]]){ //solo si vario el precio en esta consulta respecto a la ultima consulta
+                                console.log(currencyName[i] + ' vario de precio');
+                                salidaChange[currencyName[i]]=currencyOut[currencyName[i]]-oldCurrency[currencyName[i]]; //encuentro la diferencia por tipo moneda
+                                oldCurrency[currencyName[i]]=currencyOut[currencyName[i]]; //paso el nuevo valor al old para la nueva consulta
                             }
                             else { //solo de control pero igual guardo en el old
-                                console.log(nameMoneda[i] + ' no vario de precio'); 
-                                oldCurrency[nameMoneda[i]]=salidaCurrency[nameMoneda[i]]; //paso el nuevo valor al old para la nueva consulta
+                                console.log(currencyName[i] + ' no vario de precio'); 
+                                oldCurrency[currencyName[i]]=currencyOut[currencyName[i]]; //paso el nuevo valor al old para la nueva consulta
                             }
                         }
                         else {
-                             console.log('no existe ' + nameMoneda[i] + ' en oldcurrency');
-                            oldCurrency[nameMoneda[i]] = salidaCurrency[nameMoneda[i]]; //creo en old el valor del new para la proxima consulta
+                             console.log('no existe ' + currencyName[i] + ' en oldcurrency');
+                            oldCurrency[currencyName[i]] = currencyOut[currencyName[i]]; //creo en old el valor del new para la proxima consulta
                             console.log(oldCurrency);
                         }
                     }
@@ -74,7 +72,7 @@ export default {
                     // q tal si aqui guardamos en db
                 // return [
                     // { 
-                        // Rates: salidaCurrency,
+                        // Rates: currencyOut,
                         // Variations: salidaChange
                     // }
                 //] // armo mi array con objs de salida
